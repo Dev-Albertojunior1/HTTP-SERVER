1f863a20036b83ff30fadb08a9aff872e69c3ef2
Alberto Junior <albertojunior848423803@gmail.com>
2024-10-12 12:35:25 +0300

    this is working

--- router/router.go
+++ router/router.go
@@ -4,8 +4,39 @@ import (
 	"context"
 	"net/http"
 	"strings"
+
+	"honnef.co/go/tools/pattern"
 )
 
 type Router struct {
 	routes map[string]http.HandlerFunc
 }
+
+func NewRouter() *Router {
+
+	return &Router{
+		routes: make(map[string]http.HandlerFunc),
+	}
+}
+
+
+func (r *Router) HandleFunc(method, pattern string, handler http.HandlerFunc) {
+	if r.routes[method] == nil {
+		r.routes[method] = make(map[string]http.HandlerFunc)
+	}
+	r.routes[method][pattern] = handler
+}
+
+func (r *Router) GET(pattern string, handler http.HandlerFunc){
+  r.HandleFunc(http.MethodGet, pattern, handler)
+}
+
+
+func (r *Router) POST(pattern string, handler http.HandlerFunc){
+  r.HandleFunc((http.MethodPost, pattern, handler)
+}
+
+func (r *Router) DELETE(pattern string, handler http.HandlerFunc){
+
+}
+
