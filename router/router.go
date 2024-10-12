134499058cb462a3d27b75f44062592611933b46
Alberto Junior <albertojunior848423803@gmail.com>
2024-10-12 13:02:59 +0300

    this is working

--- router/router.go
+++ router/router.go
@@ -19,7 +19,6 @@ func NewRouter() *Router {
 	}
 }
 
-
 func (r *Router) HandleFunc(method, pattern string, handler http.HandlerFunc) {
 	if r.routes[method] == nil {
 		r.routes[method] = make(map[string]http.HandlerFunc)
@@ -27,16 +26,40 @@ func (r *Router) HandleFunc(method, pattern string, handler http.HandlerFunc) {
 	r.routes[method][pattern] = handler
 }
 
-func (r *Router) GET(pattern string, handler http.HandlerFunc){
-  r.HandleFunc(http.MethodGet, pattern, handler)
+func (r *Router) GET(pattern string, handler http.HandlerFunc) {
+	r.HandleFunc(http.MethodGet, pattern, handler)
 }
 
-
-func (r *Router) POST(pattern string, handler http.HandlerFunc){
-  r.HandleFunc((http.MethodPost, pattern, handler)
+func (r *Router) POST(pattern string, handler http.HandlerFunc) {
+	r.HandleFunc(http.MethodPost, pattern, handler)
 }
 
-func (r *Router) DELETE(pattern string, handler http.HandlerFunc){
-
+func (r *Router) DELETE(pattern string, handler http.HandlerFunc) {
+	r.HandleFunc(http.MethodDelete, pattern, handler)
 }
 
+func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	if handler, params, found := r.findHandler(req.Method, req.URL.Path); found {
+		ctx := req.Context()
+		for k, v := range params {
+			ctx = context.WithValue(ctx, k, v)
+		}
+
+		handler.ServeHTTP(w, req.WithContext(ctx))
+		return
+	}
+	http.NotFound(w, req)
+}
+
+func (r *Router) findHandler(method, path string) (http.HandlerFunc, map[string]string, bool) {
+
+	for pattern, handler := range r.routes[method] {
+		params, found := match(pattern, path)
+		if found {
+			return handler, params, true
+		}
+	}
+	return nil, nil, false
+}
+
+func match(pattern)
